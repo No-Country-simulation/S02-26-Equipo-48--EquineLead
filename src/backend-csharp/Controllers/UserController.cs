d1d8ea4d4cbbe4000691daf1496b2abdcdae19ca
Isabel Marquinez <isamarquinez.1998@gmail.com>
2026-02-18 15:56:37 -0500

    feat: initial backend upload with current progress

---
+++ src/backend-csharp/Controllers/UserController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project_No_Country_E48.Data;
+using Project_No_Country_E48.Models;
+using static Project_No_Country_E48.Data.AppDbContex;
+
+namespace Project_No_Country_E48.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        //Metodo Get 
+        [HttpGet] //Obtener todos
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]//Obtener por ID
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        //Metodo Post
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] User user)
+        {
+            user.UserCreatedAt = DateTime.UtcNow;
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        //Metodo Put (Update-Actualizar)
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, [FromBody] User updatedUser)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+                return NotFound();
+
+            // actualizar campos
+            user.UserName = updatedUser.UserName;
+            user.UserPhone = updatedUser.UserPhone;
+            user.UserEmail = updatedUser.UserEmail;
+            user.UserCity = updatedUser.UserCity;
+            user.UserBudget = updatedUser.UserBudget;
+            user.UserType = updatedUser.UserType;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        //Metodo Delete
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+                return NotFound();
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ src/backend-csharp/Models/LeadScore.cs
@@ -0,0 +1,20 @@
+﻿using Project_No_Country_E48.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_No_Country_E48.Models
+{
+    public class LeadScore
+    {
+        [Key]
+        public int LeadScoreId { get; set; }
+        public int UserId   { get; set; }
+        public decimal LeadScoreValue { get; set; }
+        public ScoreClassificationEnum LeadScoreClassification { get; set; }
+        public DateTime LeadScoreDate { get; set; }
+        public string? ScoreModelVersion { get; set; }
+
+        //Relaciones
+        public User? User { get; set; }
+
+    }
+}
